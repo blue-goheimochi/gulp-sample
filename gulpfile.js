1634aba6d6537c4aab38712c48d6726e55d5c06f
Yuta Ohashi <ooohhhaaassshhhiii@gmail.com>
2015-02-27 18:30:55 +0900

    Add gulpfile.js for browser start-up

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('default', function(){
+    browserSync({
+        server: {
+            baseDir: "./public_html",
+            port: 8080
+        }
+    });
+});
